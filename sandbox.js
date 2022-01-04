21aaa4ba44fac289b301860bed21aba6491d37ae
Alex Sexwale <alexsexwale@gmail.com>
2022-01-04 06:54:39 +0200

    JSON data

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,13 @@
-//// PART 4: Callback Function ///
+//// PART 5: Using JSON Data ///
 
-// A callback is a function passed as an argument to another function.
+// JSON stands for JavaScript Object Notation
 
-//REFERENCE: https://www.w3schools.com/js/js_callback.asp
+// JSON data is string that looks like JavaScript Objects. Looks like an array of objects but we say they are strings
+
+// JSON is used to send data between computers (from the server to the client)
+
+// The difference between JSON and JavaScript objects is JSON has to use double quotations (NOT SINGLE) where JavaScript Objects we can use single quotes
+//REFERENCE: https://www.w3schools.com/js/js_json_intro.asp
 
 const getTodos = (callback) => {
     const request = new XMLHttpRequest();
@@ -10,8 +15,9 @@ const getTodos = (callback) => {
     // Request change means that it is going through different phases of the request, there are 4 different phases
     request.addEventListener('readystatechange', () => {
         if(request.readyState === 4 && request.status === 200) {
-            
-            callback(undefined, request.responseText);
+            // This method takes in JSON string and converts it into JavaScript Objects so we can then have an array of JavaScript objects
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
         }
         else if(request.readyState === 4) {
             // We are stating what the error is in the first arguement and the second arguement we are making the data undefined
@@ -19,7 +25,10 @@ const getTodos = (callback) => {
         }
     });
     
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    //request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+
+    // We can access a JSON file in our file directory too
+    request.open('GET', 'todos.json');
     request.send();
 };
 
