1071acda6521524808866197258d37956f2bfc98
Alex Sexwale <alexsexwale@gmail.com>
2022-01-04 06:36:43 +0200

    Callback function

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,42 @@
-//// PART 3: Status Codes ///
+//// PART 4: Callback Function ///
 
+// A callback is a function passed as an argument to another function.
 
-// We will be working from https://jsonplaceholder.typicode.com
+//REFERENCE: https://www.w3schools.com/js/js_callback.asp
 
-//We are creating an HTTP Request
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-// Request change means that it is going through different phases of the request, there are 4 different phases
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-        // This is the data that we are getting back
-        console.log(request, request.responseText);
-    }
-    else if(request.readyState === 4) {
-        // This is the data that we are getting back
-        console.log('could not fetch the data');
-    }
-    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+    // Request change means that it is going through different phases of the request, there are 4 different phases
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200) {
+            
+            callback(undefined, request.responseText);
+        }
+        else if(request.readyState === 4) {
+            // We are stating what the error is in the first arguement and the second arguement we are making the data undefined
+            callback('could not fetch data', undefined);
+        }
+    });
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+console.log(1);
+console.log(2);
+
+// We are creating the callback function here 
+getTodos((err, data) => {
+    console.log('callback executed');
+    // If we have an error output the error
+    if(err)
+      console.log(err);
+    // Otherwise output the data
+    else
+      console.log(data);
+    
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
-
-// Status: 404 - Bad status - cannot find the URL
-// Status 200 - Successful request
-
-
-// You can find the full list here
-
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+console.log(3);
+console.log(4);
