fe7abc3474054e47ebc0b2cc9d04745e1c15fc5d
Alex Sexwale <alexsexwale@gmail.com>
2022-01-05 07:12:09 +0200

    Throwing Errors

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,14 @@
-//// PART 10: Async & Await ////
+//// PART 11: Throwing Errors ////
 
-// Async and Await allow us to chain promises together in a clean and more readable way
-
-// Async and Await is not support in Internet Explorer but works on all modern browsers
-
-// By including the word async we are now making it an asynchronous function
 const getTodos = async () => {
-    // The await key word stops it from assigning  a value to this variable/constant until the promise has been resolved
     const response = await fetch('todos/bob.json');
-    // We need to include the await because json() is an asynchronous function so we have to wait until the data has been retrieved
+    if(response.status !== 200) {
+        throw new Error('Cannot fetch the data');
+    }
     const data = await response.json();
     return data;
 };
 
-console.log(1);
-console.log(2);
-getTodos().then(data => console.log('resolved:', data));
-console.log(3);
-console.log(4);
+getTodos()
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected', err.message));
